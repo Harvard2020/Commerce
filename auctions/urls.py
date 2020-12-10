f52dd6199e9c5d3039a899d417f0dbc6a32ecf5c
Harvard2020 <65506613+Harvard2020@users.noreply.github.com>
2020-12-09 21:26:16 -0500

    Update urls.py

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,20 +3,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+        path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("activelisting", views.activelisting, name="activelisting"),
+    path("createlisting", views.createlisting, name="createlisting"),
+    path("viewlisting/<int:product_id>", views.viewlisting, name="viewlisting"),
     path("categories", views.categories, name="categories"),
-    path("category/<str:category>", views.category, name="category"),
-    path("create", views.create, name="create"),
-    path("submit",views.submit,name="submit"),
-    path("listings/<int:id>",views.listingpage,name="listingpage"),
-    path("bidsubmit/<int:listingid>",views.bidsubmit,name="bidsubmit"),
-    path("cmntsubmit/<int:listingid>",views.cmntsubmit,name="cmntsubmit"),
-    path("addwatchlist/<int:listingid>",views.addwatchlist,name="addwatchlist"),
-    path("removewatchlist/<int:listingid>",views.removewatchlist,name="removewatchlist"),
-    path("watchlist/<str:username>",views.watchlistpage,name="watchlistpage"),
-    path("closebid/<int:listingid>",views.closebid,name="closebid"),
-    path("mywinnings",views.mywinnings,name="mywinnings")
+    path("addtowatchlist/<int:product_id>",
+         views.addtowatchlist, name="addtowatchlist"),
+    path("addcomment/<int:product_id>", views.addcomment, name="addcomment"),
+    path("category/<str:categ>", views.category, name="category"),
+    path("closebid/<int:product_id>", views.closebid, name="closebid"),
+    path("closedlisting", views.closedlisting, name="closedlisting")
 ]
